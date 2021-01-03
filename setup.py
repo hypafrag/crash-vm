242c7f14b2bc9192189ce292beaa49eab20aef26
escobar <escobar@mosk.ru>
2021-01-03 10:04:47 +0700

    removed author

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import setuptools
 setuptools.setup(
     name="crash-vm",
     version="0.0.1",
-    author="escobar",
     description="Simple virtual machine implemented in pure python",
     classifiers=[
         "Programming Language :: Python :: 3",
